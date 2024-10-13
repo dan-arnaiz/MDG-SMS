59ee61f763c018bddc6ff0eabca8b48c78086e4c
guy0man <114428222+guy0man@users.noreply.github.com>
2024-10-13 22:26:46 +0800

    Context Provider & Basic Login Page

--- mdg-sms/react/src/components/StudentLayout.jsx
+++ mdg-sms/react/src/components/StudentLayout.jsx
@@ -1,6 +1,13 @@
-import {Outlet } from "react-router-dom";
+import {Outlet, Navigate} from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function StudentLayout() {
+
+    const {user, token} = useStateContext()
+
+    if(!token) {
+        return <Navigate to="/login"/>
+    }
     
     return(
         <div>
--- mdg-sms/react/src/views/Login.jsx
+++ mdg-sms/react/src/views/Login.jsx
@@ -1,8 +1,19 @@
 export default function Login() {
-    
+
+    const onSubmit = (ev) => { 
+        ev.preventDefault()
+    }
+
     return(
-        <div>
-            Login
+        <div className="Login-Form">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="Title">Login</h1>
+                    <input type="email" placeholder="Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <button className="Btn">Login</button>
+                </form>
+            </div>
         </div>
     )
 }
