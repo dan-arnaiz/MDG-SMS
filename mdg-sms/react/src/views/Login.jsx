7b9aade65eed6330665c3862a0c74163a000816f
guy0man <114428222+guy0man@users.noreply.github.com>
2024-10-13 22:49:06 +0800

    Implemented Axiom

--- mdg-sms/react/src/views/Login.jsx
+++ mdg-sms/react/src/views/Login.jsx
@@ -1,7 +1,38 @@
+import { useRef } from "react"
+
 export default function Login() {
+
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const {setUser, setToken} = useStateContext
+
 
     const onSubmit = (ev) => { 
         ev.preventDefault()
+
+        // print username and password values in console (for testing)
+        const payload = {
+            name: nameRef.current.value,
+            password: passwordRef.current.value
+        }
+
+        axiosClient.post('/signup', payload).then(({data}) => {
+            
+            setUser(data.user)
+            setToken(data.Token)
+
+        })
+
+        .catch(err => {
+
+            const response = err.response;
+            if (response && response.status == 422){
+               
+                console.log(response.data.errors);
+            }
+            
+        })
     }
 
     return(
@@ -9,8 +40,8 @@ export default function Login() {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="Title">Login</h1>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
+                    <input ref={emailRef} type="email" placeholder="Email"/>
+                    <input ref={passwordRef} type="password" placeholder="Password"/>
                     <button className="Btn">Login</button>
                 </form>
             </div>
