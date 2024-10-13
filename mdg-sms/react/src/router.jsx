e02cea4eee2752d9588055dd085fb0b02bcea71a
guy0man <114428222+guy0man@users.noreply.github.com>
2024-10-13 21:50:56 +0800

    Added Router

---
+++ mdg-sms/react/src/router.jsx
@@ -0,0 +1,43 @@
+import {createBrowserRouter} from "react-router-dom";
+import Login from "./views/login.jsx"
+import NotFound from "./views/NotFound.jsx";
+import AdminLayout from "./components/AdminLayout.jsx";
+import StudentLayout from "./components/StudentLayout.jsx";
+import AdminDash from "./views/AdminDash.jsx";
+import StudentDash from "./views/StudentDash.jsx";
+
+const router = createBrowserRouter([
+
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    },
+    {
+        path: '/',
+        element:<AdminLayout/>,
+        children: [
+
+            {
+                path: '/adashboard',
+                element: <AdminDash />
+            }
+        ]
+    },
+    {
+        path: '/',
+        element:<StudentLayout/>,
+        children: [
+
+            {
+                path: '/sdashboard',
+                element: <StudentDash />
+            }
+        ]
+    }
+])
+
+export default router;
