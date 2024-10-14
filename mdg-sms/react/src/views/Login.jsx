ca3dcb37e524cf61149560761eee6b47ad830f7e
Restyled.io <commits@restyled.io>
2024-10-14 15:46:21 +0000

    Restyled by prettier

--- mdg-sms/react/src/views/Login.jsx
+++ mdg-sms/react/src/views/Login.jsx
@@ -1,69 +1,65 @@
-import { useRef } from "react"
+import { useRef } from "react";
 
 export default function Login() {
-
     const emailRef = useRef();
     const passwordRef = useRef();
-    const [errors, setErrors] = useState(null)
-    const {setUser, setToken} = useStateContext
+    const [errors, setErrors] = useState(null);
+    const { setUser, setToken } = useStateContext;
 
-
-    const onSubmit = (ev) => { 
-        ev.preventDefault()
+    const onSubmit = (ev) => {
+        ev.preventDefault();
 
         // print username and password values in console (for testing)
         const payload = {
             email: emailRef.current.value,
             password: passwordRef.current.value,
-        }
+        };
         console.log(payload);
 
         setErrors(null);
 
-        axiosClient.post('/login', payload).then(({data}) => {
-            
-            setUser(data.user)
-            setToken(data.Token)
+        axiosClient
+            .post("/login", payload)
+            .then(({ data }) => {
+                setUser(data.user);
+                setToken(data.Token);
+            })
 
-        })
-
-        .catch(err => {
-
-            const response = err.response;
-            if (response && response.status == 422){
-                
-                if (response.data.errors) {
-                    console.log(response.data.errors);
-                } else {
-
-                    setErrors({
-                        email: [reponse.data.message]
-                    })
-
+            .catch((err) => {
+                const response = err.response;
+                if (response && response.status == 422) {
+                    if (response.data.errors) {
+                        console.log(response.data.errors);
+                    } else {
+                        setErrors({
+                            email: [reponse.data.message],
+                        });
+                    }
                 }
-            }
-            
-        })
-    }
+            });
+    };
 
-    return(
+    return (
         <div className="Login-Form">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="Title">Login</h1>
-                    {errors && <div className="alert">
-                    
-                        {Object.kets(errors).map(ley => (
-                            <p key={key}>{errors[key][0]}</p>
-                        ))}
-
-                    </div>
-                    }
-                    <input ref={emailRef} type="email" placeholder="Email"/>
-                    <input ref={passwordRef} type="password" placeholder="Password"/>
+                    {errors && (
+                        <div className="alert">
+                            {Object.kets(errors).map((ley) => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    )}
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
                     <button className="Btn">Login</button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
--- mdg-sms/react/src/views/StudentAccount.jsx
+++ mdg-sms/react/src/views/StudentAccount.jsx
@@ -1,63 +1,66 @@
-import { useRef, useState } from "react"
+import { useRef, useState } from "react";
 
 export default function StudentAccount() {
-
     const emailRef = useRef();
     const passwordRef = useRef();
     const paswordConfirmationRef = useRef();
 
-    const [errors, setErrors] = useState(null)
-    const {setUser, setToken} = useStateContext
+    const [errors, setErrors] = useState(null);
+    const { setUser, setToken } = useStateContext;
 
-
-    const onSubmit = (ev) => { 
-        ev.preventDefault()
+    const onSubmit = (ev) => {
+        ev.preventDefault();
 
         // print username and password values in console (for testing)
         const payload = {
             email: emailRef.current.value,
             password: passwordRef.current.value,
             password_confirmation: passwordConfirmationRef.current.value,
-        }
+        };
         console.log(payload);
 
-        axiosClient.post('/signup', payload).then(({data}) => {
-            
-            setUser(data.user)
-            setToken(data.Token)
+        axiosClient
+            .post("/signup", payload)
+            .then(({ data }) => {
+                setUser(data.user);
+                setToken(data.Token);
+            })
 
-        })
+            .catch((err) => {
+                const response = err.response;
+                if (response && response.status == 422) {
+                    console.log(response.data.errors);
+                    setErrors(response.data.errors);
+                }
+            });
+    };
 
-        .catch(err => {
-
-            const response = err.response;
-            if (response && response.status == 422){
-               
-                console.log(response.data.errors);
-                setErrors(response.data.errors)
-            }
-            
-        })
-    }
-
-    return(
+    return (
         <div className="Signup-Form">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="Title">Student Scholarship Account</h1>
-                    {errors && <div className="alert">
-                    
-                        {Object.kets(errors).map(ley => (
-                            <p key={key}>{errors[key][0]}</p>
-                        ))}
-                    </div>
-                    }
-                    <input ref={emailRef} type="email" placeholder="Email"/>
-                    <input ref={passwordRef} type="password" placeholder="Password"/>
-                    <input ref={passwordConfirmationRef} type="password" placeholder="Password"/>
+                    {errors && (
+                        <div className="alert">
+                            {Object.kets(errors).map((ley) => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    )}
+                    <input ref={emailRef} type="email" placeholder="Email" />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        ref={passwordConfirmationRef}
+                        type="password"
+                        placeholder="Password"
+                    />
                     <button className="Btn">Login</button>
                 </form>
             </div>
         </div>
-    )
+    );
 }
