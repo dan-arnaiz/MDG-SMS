4b9daffbaf5bbe7806c149b8f3b8f9064154d2cc
guy0man <114428222+guy0man@users.noreply.github.com>
2024-10-14 23:37:17 +0800

    coded login and signup request

--- mdg-sms/app/Http/Controllers/Api/AuthController.php
+++ mdg-sms/app/Http/Controllers/Api/AuthController.php
@@ -9,6 +9,31 @@ class AuthController extends Controller
 {
     public function login(LoginRequest $request){
 
+        $data = $request->validated();
+        if (!Auth::attempt($credentials)) {
+            
+            return response([
+                'message' => 'Provided email address or password is incorrect'
+            ], 422);
+        }
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response(compact('user','token'));
+    }
+    public function signup(SignuRequest $request){
 
+        $data = $request->validated();
+        /** @var \App\Models\User $user */
+
+        $user = User::create([
+
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+        
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user','token'));
     }
 }
--- mdg-sms/react/src/views/StudentAccount.jsx
+++ mdg-sms/react/src/views/StudentAccount.jsx
@@ -1,9 +1,11 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
-export default function Login() {
+export default function StudentAccount() {
 
     const emailRef = useRef();
     const passwordRef = useRef();
+    const paswordConfirmationRef = useRef();
+
     const [errors, setErrors] = useState(null)
     const {setUser, setToken} = useStateContext
 
@@ -15,12 +17,11 @@ export default function Login() {
         const payload = {
             email: emailRef.current.value,
             password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
         }
         console.log(payload);
 
-        setErrors(null);
-
-        axiosClient.post('/login', payload).then(({data}) => {
+        axiosClient.post('/signup', payload).then(({data}) => {
             
             setUser(data.user)
             setToken(data.Token)
@@ -31,36 +32,29 @@ export default function Login() {
 
             const response = err.response;
             if (response && response.status == 422){
-                
-                if (response.data.errors) {
-                    console.log(response.data.errors);
-                } else {
-
-                    setErrors({
-                        email: [reponse.data.message]
-                    })
-
-                }
+               
+                console.log(response.data.errors);
+                setErrors(response.data.errors)
             }
             
         })
     }
 
     return(
-        <div className="Login-Form">
+        <div className="Signup-Form">
             <div className="form">
                 <form onSubmit={onSubmit}>
-                    <h1 className="Title">Login</h1>
+                    <h1 className="Title">Student Scholarship Account</h1>
                     {errors && <div className="alert">
                     
                         {Object.kets(errors).map(ley => (
                             <p key={key}>{errors[key][0]}</p>
                         ))}
-
                     </div>
                     }
                     <input ref={emailRef} type="email" placeholder="Email"/>
                     <input ref={passwordRef} type="password" placeholder="Password"/>
+                    <input ref={passwordConfirmationRef} type="password" placeholder="Password"/>
                     <button className="Btn">Login</button>
                 </form>
             </div>
