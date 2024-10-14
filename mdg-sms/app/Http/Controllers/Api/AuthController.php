f30ab3ab6a897d49f62fe910252273c1cf410877
Restyled.io <commits@restyled.io>
2024-10-14 15:46:19 +0000

    Restyled by php-cs-fixer

--- mdg-sms/app/Http/Controllers/Api/AuthController.php
+++ mdg-sms/app/Http/Controllers/Api/AuthController.php
@@ -7,11 +7,12 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request){
+    public function login(LoginRequest $request)
+    {
 
         $data = $request->validated();
         if (!Auth::attempt($credentials)) {
-            
+
             return response([
                 'message' => 'Provided email address or password is incorrect'
             ], 422);
@@ -19,9 +20,10 @@ class AuthController extends Controller
         /** @var User $user */
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
-        return response(compact('user','token'));
+        return response(compact('user', 'token'));
     }
-    public function signup(SignuRequest $request){
+    public function signup(SignuRequest $request)
+    {
 
         $data = $request->validated();
         /** @var \App\Models\User $user */
@@ -31,9 +33,9 @@ class AuthController extends Controller
             'email' => $data['email'],
             'password' => bcrypt($data['password'])
         ]);
-        
+
         $token = $user->createToken('main')->plainTextToken;
 
-        return response(compact('user','token'));
+        return response(compact('user', 'token'));
     }
 }
