f547938ae4a3f1bcf198eccc55a96b3b3d63a3e0
dan-arnaiz <dfarnaiz@gmail.com>
2024-10-26 03:30:25 +0800

    Config CORS

--- mdg-sms/app/Http/Controllers/Api/AuthController.php
+++ mdg-sms/app/Http/Controllers/Api/AuthController.php
@@ -4,36 +4,43 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request){
-
+    public function login(LoginRequest $request)
+    {
         $data = $request->validated();
+        $credentials = ['email' => $data['email'], 'password' => $data['password']];
+
         if (!Auth::attempt($credentials)) {
-            
             return response([
                 'message' => 'Provided email address or password is incorrect'
             ], 422);
         }
+
         /** @var User $user */
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
-        return response(compact('user','token'));
+
+        return response(compact('user', 'token'));
     }
-    public function signup(SignuRequest $request){
 
+    public function signup(SignupRequest $request)
+    {
         $data = $request->validated();
+
         /** @var \App\Models\User $user */
-
         $user = User::create([
-
             'email' => $data['email'],
             'password' => bcrypt($data['password'])
         ]);
-        
+
         $token = $user->createToken('main')->plainTextToken;
 
-        return response(compact('user','token'));
+        return response(compact('user', 'token'));
     }
 }
--- mdg-sms/routes/api.php
+++ mdg-sms/routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/login', [AuthController::class], 'login');
-Route::post('/signup', [AuthController::class], 'signup');
+Route::middleware('api')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/signup', [AuthController::class, 'signup']);
+    
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
