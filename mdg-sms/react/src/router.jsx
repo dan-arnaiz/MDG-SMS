4f1dc37124724880fbc27790647b9d517e2bd82f
dan-arnaiz <dfarnaiz@gmail.com>
2024-10-28 02:02:08 +0800

    Added Temp SignUp Override // Added bcrypt password. // State Management.

--- mdg-sms/react/src/router.jsx
+++ mdg-sms/react/src/router.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import AdminLayout from "./components/AdminLayout.jsx";
 import StudentLayout from "./components/StudentLayout.jsx";
 import AuthLayout from "./components/AuthLayout.jsx";
-
+import Signup from "./views/Signup.jsx"; // Import the Signup component::Just for Overriding
 import Login from "./views/Login.jsx";
 import NotFound from "./views/NotFound.jsx";
 import AdminDash from "./views/AdminDash.jsx";
@@ -59,7 +59,13 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+                // Override the Signup component
             }]
+            
     },
     {
         path: '*',
