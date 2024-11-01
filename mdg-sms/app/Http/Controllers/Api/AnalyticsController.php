6d3b20e311527dca61ebd17235e10ec4180a9054
guy0man <114428222+guy0man@users.noreply.github.com>
2024-11-01 15:35:08 +0800

    Analytics UI

--- mdg-sms/app/Http/Controllers/Api/AnalyticsController.php
+++ mdg-sms/app/Http/Controllers/Api/AnalyticsController.php
@@ -41,36 +41,4 @@ class AnalyticsController extends Controller
             return response()->json(['error' => 'Something went wrong'], 500);
         }
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-
-    }
 }
