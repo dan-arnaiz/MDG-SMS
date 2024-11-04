455cabdfb6ad3dab6a2bcc2901ca152133aa0b58
dan-arnaiz <dfarnaiz@gmail.com>
2024-11-04 13:31:24 +0800

    Admin Reports

--- mdg-sms/react/src/views/AdminReports.jsx
+++ mdg-sms/react/src/views/AdminReports.jsx
@@ -1,14 +1,37 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import * as Icons from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as Icons from '@fortawesome/free-solid-svg-icons'
+import {Link, useNavigate} from "react-router-dom"
 // import { Link } from "react-router-dom"
 
+
+
 export default function AdminDash() {
+
+    const navigate = useNavigate();
+
+    
     return (
         <div>
             <div className="main">
                 <div className="header-toolbar">
-                    <h1 className='text-black font-bold font-sans text-lg'>Reports</h1>   
+                    <h1 className='text-black font-bold font-sans text-lg'>Reports</h1>
+                      
                 </div>
+                <div className="students-toolbar">
+                    <div className="students-searchbox">
+                        <div>
+                            <FontAwesomeIcon icon={Icons.faMagnifyingGlass} size='lg'/>
+                        </div>
+                        <input type="text"  placeholder="Search Students"/>
+                    </div>                    
+                    <div className="students-toolbar-btns">
+                        <button>Filter</button>
+                        <button>Import</button>
+                        <button>Export</button>
+                        <button className='addstudent-btn' onClick={() => navigate('/add-student')}>Add Student</button>
+                    </div>                              
+                </div>
+                
             <div className="admin-dash">
                 <div className="cards-container">
                     <div className="card">
